f30fb333cc1f183bf36d663d39d27e189dc8d1e1
Jim Drannbauer <excellentdrums@gmail.com>
2015-03-27 17:34:56 -0400

    initialize features too

--- lib/parallel_tests/cucumber/failures_logger.rb
+++ lib/parallel_tests/cucumber/failures_logger.rb
@@ -9,6 +9,7 @@ module ParallelTests
       def initialize(runtime, path_or_io, options)
         @io = prepare_io(path_or_io)
         @lines = {}
+        @features = {}
       end
 
       def after_feature(feature)
